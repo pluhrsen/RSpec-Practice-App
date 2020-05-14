3e9781ee7719b0f85488c27cbf60fb5155ebfc04
paulluhrsen <paulluhrsen@gmail.com>
2020-05-14 17:00:07 -0500

    initial project and task models

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,23 @@
 class Project
+  attr_accessor :tasks
+
+  def initialize
+    @tasks = []
+  end
+
+  def incomplete_tasks
+    tasks.reject(&:complete?)
+  end
+
   def done?
-    true
+    incomplete_tasks.empty?
+  end
+
+  def total_size
+    tasks.sum(&:size)
+  end
+
+  def remaining_size
+    incomplete_tasks.sum(&:size)
   end
 end
